28ed4a94d66870ca62c0a07776204ffa3f0f23e2
Gilbert Simon <si_gil@live.fr>
2020-08-13 14:39:50 +0100

    styles

--- components/Register.js
+++ components/Register.js
@@ -91,5 +91,22 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
-   
+    inputText:{
+      height:50,
+      color:"white"
+    },
+    inputView:{
+      backgroundColor:"#465881",
+      borderRadius:25,
+      height:50,
+      marginBottom:20,
+      justifyContent:"center",
+      padding:20
+    },
+    logo:{
+      fontWeight:"bold",
+      fontSize:50,
+      color:"#fb5b5a",
+      marginBottom:40
+    },
   });
