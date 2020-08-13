e9f7e9e9ff20cc44b9e237ab91c93252e8a42a8d
Gilbert Simon <si_gil@live.fr>
2020-08-13 13:23:14 +0100

    styles

--- components/Login.js
+++ components/Login.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, Button,AsyncStorage  } from 'react-native';
 import axios from 'axios';
-//import AppNavigator from './AppNavigator';
 
 
 
--- components/Register.js
+++ components/Register.js
@@ -23,38 +23,5 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
-    logo:{
-      fontWeight:"bold",
-      fontSize:50,
-      color:"#fb5b5a",
-      marginBottom:40
-    },
-    inputView:{
-      backgroundColor:"#465881",
-      borderRadius:25,
-      height:50,
-      marginBottom:20,
-      justifyContent:"center",
-      padding:20
-    },
-    inputText:{
-      height:50,
-      color:"white"
-    },
-    forgot:{
-      color:"white",
-      fontSize:11
-    },
-    loginBtn:{
-      backgroundColor:"#fb5b5a",
-      borderRadius:25,
-      height:50,
-      alignItems:"center",
-      justifyContent:"center",
-      marginTop:40,
-      marginBottom:10
-    },
-    loginText:{
-      color:"white"
-    }
+   
   });
